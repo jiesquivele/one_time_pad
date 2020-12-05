5d36fd37e5fe7b69efa2f63687f15701b7eda04d
Ignacio Esquivel <jiesquivele@gmail.com>
2020-12-05 11:32:39 -0600

    Added read_user_input function.

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,32 @@ use std::io;
 
 fn main() {
 
-    let mut message = String::new();
-    println!("Go ahead, type the message you wish to encrypt:");
-    io::stdin()
-        .read_line(&mut message)
-        .expect("Failed to read message.");
-    println!("The message is: {}", message);
+    // READS THE MESSAGE
+    println!("Go ahead, type the message you wish to encrypt: ");
+    let mut message = read_user_input();
+    println!("The messasge you typed is:  {}", message);
 
-    let mut key = String::new();
-    println!("Now, type a key to encrypt the message above (Note: key must be of the same length as the message):");
-    io::stdin()
-        .read_line(&mut key)
-        .expect("Failed to read key.");
-    println!("The key is: {}", key);
+    // READS THE KEY
+    println!("Now, type a key to encrypt the message above (Note: the key must be of the same length as the message): ");
+    let mut key = read_user_input();
+    println!("The key you typed is:  {}", key);
 
-    let mut ciphertext= encrypt(&mut key, &mut message);
+    // ENCRYPTS THE MESSAGE
+    let mut ciphertext = encrypt(&mut key, &mut message);
     println!("The message has been encrypted. The resulting ciphertext is: {}", ciphertext);
 
+    // DECRYPTS THE MESSAGE
     let decrypted_message = decrypt(&mut key, &mut ciphertext);
     println!("The message has been decrypted. It says: {}", decrypted_message);
+}
+
+// FUNCTION 0. READS USER INPUT
+fn read_user_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .unwrap();
+    input
 }
 
 // FUNCTION 1. CONVERTS TEXT TO BYTES
@@ -29,7 +36,7 @@ fn text_to_bytes(text: &mut String) -> &[u8] {
 }
 
 // FUNCTION 2. PERFORMS XOR BETWEEN TWO ARRAYS OF BYTES
-fn apply_xor(key_bytes: &[u8],text_bytes: &[u8]) -> Vec<u8> {
+fn apply_xor(key_bytes: &[u8], text_bytes: &[u8]) -> Vec<u8> {
     let mut total_values_xor: Vec<u8> = Vec::new();
     for element in 0..key_bytes.len() {
         let individual_value_xor = key_bytes[element] ^ text_bytes[element];
@@ -48,7 +55,7 @@ fn bytes_to_text(text: Vec<u8>) -> String {
 fn encrypt(key: &mut String, message: &mut String) -> String {
     let message_bytes = text_to_bytes(message);
     let key_bytes = text_to_bytes(key);
-    let ciphertext_bytes = apply_xor(key_bytes,message_bytes);
+    let ciphertext_bytes = apply_xor(key_bytes, message_bytes);
     let ciphertext = bytes_to_text(ciphertext_bytes);
     ciphertext
 }
@@ -57,7 +64,7 @@ fn encrypt(key: &mut String, message: &mut String) -> String {
 fn decrypt(key: &mut String, ciphertext: &mut String) -> String {
     let ciphertext_bytes = text_to_bytes(ciphertext);
     let key_bytes = text_to_bytes(key);
-    let message_bytes = apply_xor(key_bytes,ciphertext_bytes);
+    let message_bytes = apply_xor(key_bytes, ciphertext_bytes);
     let message = bytes_to_text(message_bytes);
     message
 }
